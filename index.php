1165819d90b7c3098726efb81a96598866f53094
Mike B Harris <mike.harris@xtreamlab.net>
2016-10-22 22:50:37 +0100

    Uploaded core files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Page Title</title>
+  <?php require "head.html" ?>
+</head>
+
+<body>
+  <?php require "header.html" ?>
+
+  <article id="section1">
+    <h2>Heading</h2>
+    <p>Paragraph</p>
+    <h3>Sub Heading</h3>
+  </article>
+
+  <article id="section2">
+    <h2>Heading</h2>
+    <p>Paragraph</p>
+    <h3>Sub Heading</h3>
+  </article>
+
+  <?php require "footer.html" ?>
+</body>
+</html>
